05b0d76d6079c155dd9c82525ddf8fb755a51ce0
jaweriadev <jaweriaabdulkhaliq@gmail.com>
2024-01-17 10:49:30 +0500

    loader

--- script.js
+++ script.js
@@ -7,3 +7,10 @@ function closeContent() {
   var content = document.getElementById("content");
   content.style.display = "none";
 }
+// Wait for the window to load
+window.addEventListener("load", function () {
+  // After 3 seconds, remove the loader
+  setTimeout(function () {
+    document.getElementById("loader").style.display = "none";
+  }, 3000);
+});
