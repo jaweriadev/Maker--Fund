b719607ede56f3e4e495aec718a63ee2443133cd
jaweriadev <jaweriaabdulkhaliq@gmail.com>
2024-01-25 14:51:00 +0500

    footer

--- script.js
+++ script.js
@@ -14,3 +14,23 @@ window.addEventListener("load", function () {
     document.getElementById("loader").style.display = "none";
   }, 3000);
 });
+// by clicking icons content will change
+// function changeContent(newHeading, newContent) {
+//   document.getElementById("heading").innerText = newHeading;
+//   document.getElementById("content2").innerText = newContent;
+// }
+// footer
+function scrollToTop() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+}
+
+// Show the scroll-to-top button when scrolling down
+window.onscroll = function () {
+  var scrollButton = document.querySelector(".scroll-to-top");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollButton.style.display = "block";
+  } else {
+    scrollButton.style.display = "none";
+  }
+};
